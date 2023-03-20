e90792b7f6c7e7838dc210edb2ebc75cffd22258
Chakhkiev Tamerlan <timur-ch@yandex.ru>
2023-03-20 17:13:04 +0300

    Mesto1

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+const popup = document.querySelector('.popup');
+const editButton = document.querySelector('.profile__edit-button');
+const closeButton = document.querySelector('.popup__close');
+const profileName = document.querySelector('.profile__name');
+const profileAbout = document.querySelector('.profile__about');
+const nameInput = document.querySelector('.popup__input_name');
+const aboutInput = document.querySelector('.popup__input_about');
+const formElement = document.querySelector('.popup__form');
+
+function changeValue() {
+    nameInput.value = profileName.textContent;
+    aboutInput.value = profileAbout.textContent;
+}
+
+function openedPopup() {
+    popup.classList.add('popup__opened');
+    changeValue();
+}
+
+function closePopup() {
+    popup.classList.remove('popup__opened');
+}
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileAbout.textContent = aboutInput.value;
+    closePopup();
+}
+
+
+editButton.addEventListener('click', openedPopup);
+closeButton.addEventListener('click', closePopup);
+formElement.addEventListener('submit', handleFormSubmit);
